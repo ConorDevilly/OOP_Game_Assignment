3fed70f6fbee41d7586b8217ee3cc6329376d4ec
Conor Devilly <c14311816@mydit.ie>
2016-01-24 14:16:56 +0000

    Wave HUD

--- src/fpsGame/HUD.java
+++ src/fpsGame/HUD.java
@@ -7,11 +7,13 @@ public class HUD extends GameObject{
 	PApplet p;
 	int score;
 	int shield;
+	int wave;
 	
 	HUD(PApplet p){
 		this.p = p;
 		score = 0;
 		shield = 10;
+		wave = 1;
 	}
 	
 	@Override
@@ -30,6 +32,7 @@ public class HUD extends GameObject{
 		p.textAlign(PApplet.CENTER, PApplet.TOP);
 		p.textSize(26);
 		p.text("SCORE", p.width / 6, 10, 1);
+		p.fill(0, 255, 0);
 		p.text(score, p.width / 6, 10 + p.textAscent() + p.textDescent(), 1);
 		
 		//Shield
@@ -44,8 +47,15 @@ public class HUD extends GameObject{
 		p.vertex(p.width / 3, 10, 1);
 		p.endShape();
 		p.fill(0, 255, 0);
-		p.text(shield, p.width / 2, 20, 1);
-		p.text("SHIELD", p.width / 2, 20 + p.textDescent() + p.textAscent(), 1);
+		p.text(shield, p.width / 2, 10, 1);
+		p.text("SHIELD", p.width / 2, 10 + p.textDescent() + p.textAscent(), 1);
+		
+		//Wave
+		p.text(wave, 5 * p.width / 6, 10, 1);
+		p.fill(255, 0, 0);
+		p.textAlign(PApplet.LEFT, PApplet.TOP);
+		p.text("WAVE", 5 * p.width / 6 + p.textWidth(Integer.toString(wave)), 10, 1);
+		
 		
 		p.popStyle();
 		p.popMatrix();
